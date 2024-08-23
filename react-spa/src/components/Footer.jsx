9b4ac5c17289949275e8800c322c9c0aec358e7f
Chathumal Navanjana <chathumalnavanjana@gmail.com>
2024-08-24 05:09:43 +0530

    Update Footer.jsx

--- react-spa/src/components/Footer.jsx
+++ react-spa/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#310] text-white tracking-[5px] py-4 flex justify-center items-center">
+    <footer className="bg-[#310] text-white tracking-[5px] py-4 flex justify-center items-center h-40">
       <p>&copy; 2024 Drogon Design. All rights reserved.</p>
     </footer>
   );
